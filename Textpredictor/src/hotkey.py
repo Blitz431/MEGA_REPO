3319cf0d05c32e677d700336b679d46644125ed4
Aarnav <arnie.saxena@gmail.com>
2026-06-29 21:48:26 -0700

    Adding the Hotkey code it works now jsut wnat it to function whereever so that i dont have to click on the file first need to figure that out next is tracker

--- Textpredictor/src/hotkey.py
+++ Textpredictor/src/hotkey.py
@@ -1,33 +1,31 @@
-import keyboard 
+import keyboard
 import time
 
-tracking_state = False  # Variable to track the state (True or False)
-keys_to_count = {'ctrl', 'alt', 'f12'}  # Keys to count to activate the portal
-held_keys = set()  # Set to store keys currently held down
-sequence = []  # List to store the sequence of keys pressed
-last_key_time = 0  # Variable to track the time of the last key press
+tracking_state = False
+keys_to_count = {'ctrl', 'alt', 'f12'}
+held_keys = set()
+last_press_time = 0
 
 def on_key_press(e):
-    global tracking_state, held_keys
+    global tracking_state, last_press_time
     
     if e.name in keys_to_count:
         held_keys.add(e.name)
         
         if len(held_keys) == len(keys_to_count):
-            sequence.extend(sorted(held_keys))
+            current_time = time.time()
             
-            if len(sequence) >= 2 * len(keys_to_count) and all(sequence[i] == sequence[i + len(keys_to_count)] for i in range(len(keys_to_count))):
-                tracking_state = not tracking_state  # Toggle the state
-                print("Portal activated!")  # Perform the action when the portal is active
-                
-                # Reset the sequence for next use
-                sequence.clear()
+            if current_time - last_press_time < 0.5:
+                tracking_state = not tracking_state
+                print("Portal activated!" if tracking_state else "Portal deactivated!")
+                last_press_time = 0
+            else:
+                last_press_time = current_time
             
             held_keys.clear()
 
 def on_key_release(e):
-    if e.name in keys_to_count:
-        held_keys.discard(e.name)
+    held_keys.discard(e.name)
 
 keyboard.on_press(on_key_press)
 keyboard.on_release(on_key_release)
